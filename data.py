8d2ef582b29eca418c60591f7604dcfc8bc94bf0
divyajeettt <divyajeet21529@iiitd.ac.in>
2023-04-19 15:33:04 +0530

    Comments

--- data.py
+++ data.py
@@ -3,7 +3,18 @@ import numpy as np
 
 
 class Dataset:
+    """
+    A class to load and preprocess the dataset.
+    :param path: The path to the csv file.
+    """
+
     def __init__(self, path):
+        """
+        __init__ self
+        The initialized object checks if the data is training type or testing type
+        and parses labels accordingly.
+        Note: The training data should have a column named "category" and the testing data should not.
+        """
         df = pd.read_csv(path, index_col=0)
         self.labels = None
         self.y = None
@@ -19,16 +30,28 @@ class Dataset:
             self.x = df.values
 
     def to_one_hot(self):
+        """
+        Converts the output to be predicted to one-hot encoded vectors.
+        """
         if self.y is None:
             raise ValueError("No labels found in dataset")
         else:
             self.y = np.eye(self.num_classes)[self.y]
 
     def get_cat_to_label(self):
+        """
+        Returns a dictionary mapping the numerical labels to their original textual representation.
+        """
         return dict(zip(self.labels.values(), self.labels.keys()))
 
 
 def write_to_csv(path, labels, mapping):
+    """
+    Writes the predictions to a csv file.
+    :param path: The path to the csv file.
+    :param labels: The labels to be written.
+    :param mapping: A dictionary mapping the numerical labels to their original textual representation.
+    """
     df = pd.DataFrame({
         "Id": list(range(len(labels))),
         "Category": [mapping[i] for i in labels]
